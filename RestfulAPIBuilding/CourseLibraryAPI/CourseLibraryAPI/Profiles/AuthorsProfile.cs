1ce3d6aa010bfae4b002c5db2cc7a2169d83445d
mpngu <mpngu>
2020-08-04 08:33:45 +0700

    Map with custom format

---
+++ RestfulAPIBuilding/CourseLibraryAPI/CourseLibraryAPI/Profiles/AuthorsProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseLibrary.API.Entities;
+using CourseLibraryAPI.Helpers;
+
+namespace CourseLibraryAPI.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        public AuthorsProfile()
+        {
+            CreateMap<Author, Models.AuhorDto>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(
+                dest => dest.Age,
+                opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge())
+                );
+        }
+    }
+}
