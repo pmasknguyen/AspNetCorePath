e27b4a01157d6a9b877e670559092b33746c5966
mpngu <nguyenmai@sucafina.com>
2020-08-11 05:29:25 +0700

    5.4 Add DbContext, ConnectionString, Configuration

--- Building Web Applications with ASP.NET Core MVC/BethanysPieShop/BethanysPieShop/Startup.cs
+++ Building Web Applications with ASP.NET Core MVC/BethanysPieShop/BethanysPieShop/Startup.cs
@@ -6,6 +6,8 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,10 +15,18 @@ namespace BethanysPieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration config)
+        {
+            this.Configuration = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options
+                => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IPieRepository, MockPieRepository>();
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
             services.AddControllersWithViews();
@@ -32,7 +42,7 @@ namespace BethanysPieShop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            
+
             app.UseNodeModules();
 
             app.UseRouting();
