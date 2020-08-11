f75ca3ee207297cff65246316fe93aafc7887b7c
mpngu <nguyenmai@sucafina.com>
2020-08-11 08:07:12 +0700

    Add home page navgation

--- Building Web Applications with ASP.NET Core MVC/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
+++ Building Web Applications with ASP.NET Core MVC/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -25,5 +25,15 @@ namespace BethanysPieShop.Controllers
             piesListViewModel.CurrentCategory = "Test";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if(pie==null)
+            {
+                return NotFound();
+            }
+            return View(pie);
+        }
     }
 }
