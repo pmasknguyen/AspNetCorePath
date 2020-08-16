d032325491050b099e5f6756bfcaa02fb37d73fc
mpngu <nguyenmai@sucafina.com>
2020-08-16 22:14:30 +0700

    Add ShoppingCartItem

---
+++ Building Web Applications with ASP.NET Core MVC/BethanysPieShop/BethanysPieShop/Models/ShoppingCartItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int ShoppingCardItemId { get; set; }
+        public Pie Pie { get; set; }
+        public int Amount { get; set; }
+        public string ShoppingCardId { get; set; }
+
+    }
+}
