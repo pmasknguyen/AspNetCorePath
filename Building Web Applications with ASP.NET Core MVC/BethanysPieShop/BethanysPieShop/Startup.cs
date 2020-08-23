13fbd1d565d7cdbb2d968df55dee0684fe29bd43
mpngu <nguyenmai@sucafina.com>
2020-08-23 17:25:39 +0700

    9.5 Add Identity page from scaffolding

--- Building Web Applications with ASP.NET Core MVC/BethanysPieShop/BethanysPieShop/Startup.cs
+++ Building Web Applications with ASP.NET Core MVC/BethanysPieShop/BethanysPieShop/Startup.cs
@@ -6,6 +6,7 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,7 @@ namespace BethanysPieShop
         {
             services.AddDbContext<AppDbContext>(options
                 => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<ShoppingCart>(p => ShoppingCart.GetCard(p));
@@ -34,6 +36,7 @@ namespace BethanysPieShop
             services.AddHttpContextAccessor();
             services.AddSession();
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,11 +54,13 @@ namespace BethanysPieShop
             app.UseNodeModules();
 
             app.UseRouting();
+            app.UseAuthentication();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
