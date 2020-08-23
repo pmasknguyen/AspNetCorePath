f9e24a201ae4fb554a60ce857415cb71c277ee5b
mpngu <nguyenmai@sucafina.com>
2020-08-23 21:08:04 +0700

    2.3 Extract interface

---
+++ Dependency Injection in ASP.NET Core/Before/src/TennisBookings.Web/Services/AmazingForecaster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisBookings.Web.Services
+{
+    public class AmazingForecaster
+    {
+        public WeatherResult GetWeatherResult()
+        {
+            return new WeatherResult {
+                WeatherCondition = WeatherCondition.Sun
+            };
+        }
+    }
+}
